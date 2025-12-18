ddaec7d5dba96f3a74b87a101284501c1c666df1
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2025-12-18 13:08:14 +0700

    commit

--- frontend/src/components/cart/CartBtn.tsx
+++ frontend/src/components/cart/CartBtn.tsx
@@ -2,10 +2,37 @@ import { ShoppingBag } from "lucide-react"
 import { Button } from "../ui/button"
 import { Badge } from "../ui/badge"
 import CartBox from "./Cart"
-import { useAppSelector } from "../../lib/hooks/redux.hook"
+import { useAppDispatch, useAppSelector } from "../../lib/hooks/redux.hook"
+import type { Cart } from "../../lib/types/cart.type"
+import { useEffect } from "react"
+import { createCart, fetchCart } from "../../lib/services/cart.service"
 
 export default function CartBtn() {
+  const dispatch = useAppDispatch()
   const cart = useAppSelector((state) => state.cart.cart)
+
+  const saveId = (payload: Cart) => {
+    const newCart = payload
+    if (newCart?.id)
+      localStorage.setItem("id", newCart.id)
+  }
+
+  useEffect(() => {
+    const id = localStorage.getItem("id")
+    console.log(id)
+    if (!id) {
+      dispatch(createCart()).then((action) => {
+        saveId(action.payload)
+      })
+    } else {
+      dispatch(fetchCart(id))
+      if (cart === null) {
+        dispatch(createCart()).then((action) => {
+          saveId(action.payload)
+        })
+      }
+    }
+  }, [dispatch])
 
   return (
     <>
